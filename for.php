fa2c440742d47cbac12d2721359585d0ac13aa75
balasss <47716534+balasss@users.noreply.github.com>
2019-09-04 18:30:22 +0530

    Create for.php

---
+++ for.php
@@ -0,0 +1,18 @@
+<?php
+for($i=1;$i<=5;$i++){
+ 
+ for ($d=5-$i; $d > 0; $d--)  {
+ 
+  echo "&nbsp;&nbsp;";
+  }
+    for($j=1;$j<=$i;$j++){
+ 
+        echo "&nbsp;".$i."&nbsp;";
+ //echo "$i";
+    }
+ 
+    echo "<br>";
+ 
+}
+
+?>
