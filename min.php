6848a8902aa04958071fea073e654fa3075cb314
balasss <47716534+balasss@users.noreply.github.com>
2019-09-13 09:36:36 +0530

    Create min.php

---
+++ min.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+<center>
+<h2>Divisible Method</h2>
+
+<form action="check.php" method="GET" >
+  Min:
+  <input type="text" name="min" >
+  <br>
+  Max:
+  <input type="text" name="max">
+  <br><br>
+  <input type="submit" value="Apply">
+</form> 
+</center>
+</body>
+</html>
